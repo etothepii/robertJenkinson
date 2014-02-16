0ae01178838052fe0ebff8e061c1a0545d59fb63
James Robinson <c9@epii.co.uk>
2014-02-16 08:06:40 -0500

    Added route

--- nodebootstrap/app.js
+++ nodebootstrap/app.js
@@ -34,6 +34,7 @@ app.get('/election', routes.election);
 app.get('/leaflet', routes.leaflet);
 app.get('/purchase', routes.purchase);
 app.get('/ureport', routes.ureport);
+app.post('/mail', routes.mail);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
